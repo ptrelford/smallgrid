35ac97ed8712e037f485123c86d182f46cbcb4da
Moon <Moon@Moon-PC>
2012-02-18 13:52:20 +0000

    Working prototype

---
+++ App.fs
@@ -0,0 +1,22 @@
+﻿namespace Ag
+
+open System.Windows
+open System.Windows.Controls
+
+type App() as this = 
+    inherit Application()
+
+    do
+        let grid = SmallGrid()
+        for x = 0 to 20 do 
+            if x % 2 = 0 then grid.Columns.Add(ColumnDefinition())
+            else grid.Columns.Add(ColumnDefinition(Width=GridLength(100.0)))
+        for y = 0 to 20 do grid.Rows.Add(RowDefinition())
+        for y = 0 to 20 do
+            for x = 0 to 20 do
+                let child = TextBlock(Text="Hello " + x.ToString() + " " + y.ToString())
+                child.SetValue(Grid.ColumnProperty, x)
+                child.SetValue(Grid.RowProperty, y)
+                grid.Children.Add child
+
+        this.Startup.AddHandler(fun o e -> this.RootVisual <- grid)
---
+++ SmallGrid.fs
@@ -0,0 +1,110 @@
+﻿namespace Ag
+
+open System
+open System.Collections.Generic
+open System.Windows
+open System.Windows.Controls
+
+module Elements =
+    let iteri f (children:UIElement seq) =
+        for child in children do
+            let element = child :?> FrameworkElement
+            let column = Grid.GetColumn(element)
+            let row = Grid.GetRow(element)
+            f column row child
+
+[<AutoOpen>]
+module Measure =
+    let measureGrid (columns:IList<ColumnDefinition>) (rows:IList<RowDefinition>) (grid:Panel) =
+        let widths = 
+            columns 
+            |> Seq.map (fun column ->
+                if column.Width.IsAbsolute 
+                then column.Width.Value
+                else 0.0
+            ) 
+            |> Seq.toArray
+
+        let heights = 
+            rows
+            |> Seq.map (fun row ->
+                if row.Height.IsAbsolute 
+                then row.Height.Value
+                else 0.0
+            ) 
+            |> Seq.toArray
+
+        let availableSize = Size(Double.PositiveInfinity, Double.PositiveInfinity)
+
+        let measureChild (child:UIElement) column row =
+            if columns.[column].Width.IsAuto || columns.[column].Width.IsStar ||
+               rows.[row].Height.IsAuto || rows.[row].Height.IsStar
+            then
+                child.Measure(availableSize)
+                if columns.[column].Width.IsAuto || columns.[column].Width.IsStar
+                then widths.[column] <- max child.DesiredSize.Width widths.[column]
+                if rows.[row].Height.IsAuto || rows.[row].Height.IsStar
+                then heights.[column] <- max child.DesiredSize.Height heights.[column]
+
+        grid.Children
+        |> Elements.iteri (fun column row child ->
+            if column >= 0 && column < columns.Count && 
+               row >= 0 && row < rows.Count 
+            then measureChild child column row
+        )
+        
+        widths, heights
+
+type SmallGrid () =
+    inherit Panel()
+
+    let columns = List<ColumnDefinition>()
+    let rows = List<RowDefinition>()
+    
+    member grid.Columns = columns :> IList<ColumnDefinition>
+    member grid.Rows = rows :> IList<RowDefinition>
+    
+    override grid.MeasureOverride(availableSize:Size) =
+        let widths, heights = measureGrid columns rows grid
+        let totalWidth =
+            columns 
+            |> Seq.mapi (fun index column ->
+                if column.Width.IsAuto || column.Width.IsStar
+                then widths.[index]
+                else column.Width.Value
+            )
+            |> Seq.sum
+        let totalHeight =
+            rows
+            |> Seq.mapi (fun index row ->
+                if row.Height.IsAuto || row.Height.IsStar
+                then heights.[index]
+                else row.Height.Value
+            )
+            |> Seq.sum
+        Size(totalWidth, totalHeight)
+
+    override grid.ArrangeOverride(finalSize:Size) =
+        let widths, heights = measureGrid columns rows grid
+        let xs = widths |> Array.scan (fun x width -> x + width) 0.0
+        let ys = heights |> Array.scan (fun y height -> y + height) 0.0
+        let dimensions column row =
+            let width =
+                if columns.[column].Width.IsAuto || columns.[column].Width.IsStar
+                then widths.[column]
+                else columns.[column].Width.Value
+            let height =
+                if rows.[row].Height.IsAuto || rows.[row].Height.IsStar
+                then heights.[row]
+                else rows.[row].Height.Value
+            width, height
+        grid.Children |> Elements.iteri (fun column row child ->
+            if column >= 0 && column < columns.Count && 
+               row >= 0 && row < rows.Count 
+            then 
+                let width, height = dimensions column row
+                let x, y = xs.[column], ys.[row]
+                Rect(x, y, width, height) 
+                |> child.Arrange
+        )
+        finalSize
