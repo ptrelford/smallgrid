9f874aa615bdf04e2bc21d064b22374512c62c15
Moon <Moon@Moon-PC>
2012-02-18 21:48:27 +0000

    Added AbsoluteGrid type for absolute dimensioned grids

---
+++ AbsoluteGrid.fs
@@ -0,0 +1,30 @@
+﻿namespace Ag
+
+open System
+open System.Collections.Generic
+open System.Windows
+open System.Windows.Controls
+
+type AbsoluteGrid () =
+    inherit Panel ()
+    let columns = List<double>()
+    let rows = List<double>()
+    
+    member grid.Columns = columns :> IList<double>
+    member grid.Rows = rows :> IList<double>
+    
+    override grid.MeasureOverride(availableSize:Size) =
+        Size(columns |> Seq.sum, rows |> Seq.sum)
+
+    override grid.ArrangeOverride(finalSize:Size) =
+        let xs = columns |> Seq.scan (+) 0.0 |> Seq.toArray
+        let ys = rows |> Seq.scan (+) 0.0 |> Seq.toArray
+        for child in grid.Children do
+            let child = child :?> FrameworkElement
+            let column = Grid.GetColumn(child)
+            let row = Grid.GetRow(child)
+            let r = Rect(xs.[column], ys.[row], columns.[column], rows.[row])
+            child.Arrange r
+        finalSize
+
+
